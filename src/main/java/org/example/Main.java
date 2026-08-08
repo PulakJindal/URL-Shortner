3489ee428b1b23defceef31ca169db18fe3f7675
Pulak Jindal <pulakjindal04@gmail.com>
2026-08-08 18:03:22 +0530

    ready to respond with short url

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,11 @@
 package org.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Optional;
 
+import io.javalin.Javalin;
+
 public class Main {
-     static void main() throws Exception {
+     public static void main(String[] args) throws Exception {
         UrlRepository repo = new UrlRepository(Database.getDataSource());
 
         boolean inserted = repo.insert("test111", "https://anthropic.com");
@@ -17,5 +13,15 @@ public class Main {
 
          Optional<String> found = repo.findByShortCode("test111");
          System.out.println("Found: "+ found.orElse("nothing"));
+
+         var app = Javalin.create(config -> {
+             config.routes.get("/", ctx -> ctx.result("Hello World"));
+             config.routes.get("/{url}", ctx ->{
+                String shortUrl = ctx.pathParam("url");
+                Optional<String> longUrl = repo.findByShortCode(shortUrl);
+                if(longUrl.isPresent()) ctx.redirect(longUrl.get());
+                else ctx.status(404);
+             });
+         }).start(7070);
     }
 }
